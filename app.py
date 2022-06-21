80bd1ae18fb8713f388a7618097f71dd9f5409fc
Sevinj-Joy <seva.rasul15@gmail.com>
2022-06-21 16:39:49 +0400

    Variables line

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-print("   /|")
-print("  / |")
-print(" /  |")
-print("/___|")
+# print("   /|")
+# print("  / |")
+# print(" /  |")
+# print("/___|")
+character_name = "John"
+character_age = "35"
+print("There was a man named " + character_name)
+print("he was 70 years old.")
+print("He really liked the name"  +" "+ character_name)
+print("But didn't like being "+ character_age)
