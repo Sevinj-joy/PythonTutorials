943b0914b9c441a40e28c3106b317dde6edd4c8d
Sevinj-Joy <seva.rasul15@gmail.com>
2022-06-21 17:23:30 +0400

    Upper function

--- app.py
+++ app.py
@@ -2,9 +2,14 @@
 # print("  / |")
 # print(" /  |")
 # print("/___|")
-character_name = "John"
-character_age = "35"
-print("There was a man named " + character_name)
-print("he was 70 years old.")
-print("He really liked the name"  +" "+ character_name)
-print("But didn't like being "+ character_age)
+# character_name = "John"
+# character_age = 35
+# is_Male = False
+# print("There was a man named " + character_name)
+# print("he was 70 years old.")
+# print("He really liked the name"  +" "+ character_name)
+# print("But didn't like being "+ character_age)
+
+phrase = "Giraffe Academy"
+# print(phrase + " "+ "is the best")
+print(phrase.isupper())
