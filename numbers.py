0e2e9de1ce443e01b427444ee01a465e07b70b6e
Sevinj-Joy <seva.rasul15@gmail.com>
2022-06-21 17:43:44 +0400

    Numbers python file updated

---
+++ numbers.py
@@ -0,0 +1,11 @@
+# print(-2.098543)
+# print(4 +5.6)
+# print(3*8+10)
+# print(3* (5+9))
+# print(10 % 3)  - qalıq
+
+# my_num = 55
+# print(str(my_num) +" " + "number is my lucky number ")
+
+my_num = -35
+print(abs(my_num))
