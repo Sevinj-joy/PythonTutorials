5f717f295809eac1247ea687740f354d17c637a0
Sevinj-Joy <seva.rasul15@gmail.com>
2022-06-23 17:22:13 +0400

    new one

--- input.py
+++ input.py
@@ -2,3 +2,8 @@ name = input("Enter your name: ")
 surname = input("Enter your surname: ")
 password = input("Enter your password: ")
 print( "Hello " + name , surname )
+
+
+price = 49
+txt = "The price is {} dollars"
+print(txt.format(price))
