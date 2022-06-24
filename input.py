31f0bbc0caa9956d8e8702e71cad339261331b55
Sevinj-Joy <seva.rasul15@gmail.com>
2022-06-24 17:00:32 +0400

    New codes running

--- input.py
+++ input.py
@@ -1,11 +1,25 @@
-name = input("Enter your name: ")
-surname = input("Enter your surname: ")
-password = input("Enter your password: ")
-age = input("Enter your age: ")
-print( "Hello " + name , surname )
-print( "HELLO " + name , + "," + "You are " + age)
+# name = input("Enter your name: ")
+# surname = input("Enter your surname: ")
+# password = input("Enter your password: ")
+# age = input("Enter your age: ")
+# print( "Hello " + name , surname )
+# print( "HELLO " + name , + "," + "You are " + age)
 
 
 # price = 49
 # txt = "The price is {} dollars"
 # print(txt.format(price))
+# number1= input("Enter first number: ")
+# number2= input("Enter second number: ")
+# result =float(number1) + float(number2)
+# print("Result is ", result)
+
+
+
+color=input("Enter a color: ")
+Plural_noun=input("Enter a plural noun: ")
+celebrity=input("Enter a celebrity: ")
+
+print(format("Roses are {color}"))
+print(format("{Plural_noun} are blue"))
+print(format("I love {celebrity}"))
