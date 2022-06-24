c2c548707c809d27e1bd4b922b093375e4532e7e
Sevinj-Joy <seva.rasul15@gmail.com>
2022-06-24 16:39:53 +0400

    Input file updated

--- input.py
+++ input.py
@@ -1,9 +1,11 @@
 name = input("Enter your name: ")
 surname = input("Enter your surname: ")
 password = input("Enter your password: ")
+age = input("Enter your age: ")
 print( "Hello " + name , surname )
+print( "HELLO " + name , + "," + "You are " + age)
 
 
-price = 49
-txt = "The price is {} dollars"
-print(txt.format(price))
+# price = 49
+# txt = "The price is {} dollars"
+# print(txt.format(price))
