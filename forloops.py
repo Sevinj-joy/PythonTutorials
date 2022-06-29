c8ba5856d9e2261d9df134ce65dcd3c0c024d978
Sevinj-Joy <seva.rasul15@gmail.com>
2022-06-29 17:25:33 +0400

    Dictionaries examples

--- forloops.py
+++ forloops.py
@@ -9,7 +9,20 @@
 #   if x == "banana":
 #     break
 
-i = 1
-while i < 19:
-  print(i)
-  i += 1
+# i = 1
+# while i < 19:
+#   print(i)
+#   i += 1
+
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"])
+thisdict.add("BMW")
+
+thisset = {"apple", "banana", "cherry"}
+thisset.add("orange")
+print(thisset)
