8ab04f361f564bd61dd2a20963ebbd2444c5185d
Sevinj-Joy <seva.rasul15@gmail.com>
2022-06-30 17:37:23 +0400

    Tuples examples

--- forloops.py
+++ forloops.py
@@ -26,3 +26,17 @@ thisdict.add("BMW")
 thisset = {"apple", "banana", "cherry"}
 thisset.add("orange")
 print(thisset)
+
+
+
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+
+print(x)
+
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.append("orange")
+thistuple = tuple(y)
